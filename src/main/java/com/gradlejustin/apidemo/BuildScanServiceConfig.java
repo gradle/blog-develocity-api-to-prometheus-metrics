43347b8e9872b9e6ee4092bd58a48c23e4e89035
Frank Zhu <frankzhu2003@gmail.com>
2024-01-19 08:52:15 -0500

    bug-fix the metric name with space in project name

--- src/main/java/com/gradlejustin/apidemo/BuildScanServiceConfig.java
+++ src/main/java/com/gradlejustin/apidemo/BuildScanServiceConfig.java
@@ -3,6 +3,8 @@ package com.gradlejustin.apidemo;
 public class BuildScanServiceConfig {
 
     //TODO: Replace the following values with your Gradle Enterprise server URL and access key
+//    final static String token = "qal3iixe457wk3ibqttrhhw7cr6snzseatlo76rlpov3mb4koavq";
+//    final static String geApiUrl = "https://ge.gradle.org/api/builds/";
     final static String token = "kmecv2f65d66n6aym74a7te6fdaqzqjj7bjaaegtveiztmkjxrnq";
     final static String geApiUrl = "https://develocity-field.gradle.com/api/builds/";
 
