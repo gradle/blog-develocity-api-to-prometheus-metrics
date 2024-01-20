c9d7dc6cab43fa23eabbc1e79fa243d50bebb878
Frank Zhu <frankzhu2003@gmail.com>
2024-01-20 17:59:12 -0500

    Add uberjar for easier execution

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ description = "API demo for Gradle Build Scans"
 plugins {
     `java-library`
     application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
@@ -19,5 +20,24 @@ dependencies {
     implementation("io.prometheus:simpleclient:0.16.0")
     implementation("io.prometheus:simpleclient_hotspot:0.16.0")
     implementation("io.prometheus:simpleclient_httpserver:0.16.0")
-    implementation("io.prometheus:simpleclient_pushgateway:0.16.0") 
+    implementation("io.prometheus:simpleclient_pushgateway:0.16.0")
 }
+
+tasks.register<Jar>("uberJar") {
+    archiveClassifier.set("uber")
+    if (project.name.equals("kotlin")) {
+        manifest.attributes["Main-Class"] = "com.gradlejustin.apidemo.BuildScanExtractorKt"
+    } else {
+        manifest.attributes["Main-Class"] = "com.gradlejustin.apidemo.BuildScanExtractor"
+    }
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath) //dependsOn(sourceSets.main.get().runtimeClasspath)
+    from({
+        configurations.implementation.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+        //sourceSets.main.get().runtimeClasspath.filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
