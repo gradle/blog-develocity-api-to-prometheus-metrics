98726406400897a48841c266e50cac452f42a7f6
Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>
2025-03-03 08:48:58 -0500

    Delete operational directory

--- operational/startUp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run --name dvme --detach --restart always --memory=2G -p 8081:8081 -e DVSERVER=ge.gradle.org -e DVKEY=ge-gradle-org.key frankzhu2003/dv-prom-metrics:latest
-
-docker run --name dvmepse --detach --restart always --memory=2G -p 8082:8081 -e DVSERVER=develocity-field.gradle.com -e DVKEY=develocity-field.key frankzhu2003/dv-prom-metrics:latest
-
-docker run --restart always --detach     --name my-prometheus     --publish 9090:9090     --volume prometheus-volume:/prometheus     --volume "$(pwd)"/prometheus.yml:/etc/prometheus/prometheus.yml  --add-host host.docker.internal=host-gateway   prom/prometheus
